e7a41e40ebae06d65438aac64cd9cd71c162de41
dpanhey <d.panhey@gmx.de>
2022-10-26 15:42:02 +0200

    add methods to start the game, set game mode, player name and sign, also a method to test variable assignment

--- fullstack_ruby/01_ruby_programming/ruby-projects/tictactoe.rb
+++ fullstack_ruby/01_ruby_programming/ruby-projects/tictactoe.rb
@@ -2,10 +2,54 @@
 
 # Top level Documentation for class TicTacToe
 class TicTacToe
-  def self.start_game; end
-  def self.set_players; end
+  def self.start_game
+    puts 'Welcome to Tic Tac Toe!'
+    set_game_mode
+  end
+
+  def self.set_game_mode
+    @computer_mode = true
+    puts 'At first, lets set the game mode.'
+    puts 'If you want to play against another Player, type Y:'
+    @computer_mode = false if gets.chomp.downcase == 'y'
+    set_player_name
+  end
+
+  def self.set_player_name
+    puts 'To start, please insert your Name:'
+    @player1_name = gets.chomp
+    unless @computer_mode
+      puts 'Whats the name of the second Player?'
+      @player2_name = gets.chomp
+    end
+    set_player_sign
+  end
+
+  def self.set_player_sign
+    puts "Wich sign do you want to have #{@player1_name}? X or O?"
+    if gets.chomp.downcase == 'x'
+      @player1_sign = 'X'
+      @computer_mode ? @computer_sign = 'O' : @player2_sign = 'O'
+    else
+      @player1_sign = 'O'
+      @computer_mode ? @computer_sign = 'X' : @player2_sign = 'X'
+    end
+  end
+
   def self.display_board; end
   def self.make_turn; end
   def self.check_win_condition; end
   def self.end_game; end
+
+  def self.test_assignment
+    puts @player1_name
+    puts @player1_sign
+    puts @player2_name
+    puts @player2_sign
+    puts @computer_mode
+    puts @computer_sign
+  end
 end
+
+TicTacToe.start_game
+TicTacToe.test_assignment
