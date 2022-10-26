4e4aae37136828e4cc11cfb21d495c2be0934075
dpanhey <d.panhey@gmx.de>
2022-10-26 21:10:47 +0200

    added a board array and the display_board method and changed the unless statement to a guard clause

--- fullstack_ruby/01_ruby_programming/ruby-projects/tictactoe.rb
+++ fullstack_ruby/01_ruby_programming/ruby-projects/tictactoe.rb
@@ -5,6 +5,9 @@ class TicTacToe
   def self.start_game
     puts 'Welcome to Tic Tac Toe!'
     set_game_mode
+    set_player_name
+    set_player_sign
+    board
   end
 
   def self.set_game_mode
@@ -12,17 +15,15 @@ class TicTacToe
     puts 'At first, lets set the game mode.'
     puts 'If you want to play against another Player, type Y:'
     @computer_mode = false if gets.chomp.downcase == 'y'
-    set_player_name
   end
 
   def self.set_player_name
     puts 'To start, please insert your Name:'
     @player1_name = gets.chomp
-    unless @computer_mode
-      puts 'Whats the name of the second Player?'
-      @player2_name = gets.chomp
-    end
-    set_player_sign
+    return if @computer_mode
+
+    puts 'Whats the name of the second Player?'
+    @player2_name = gets.chomp
   end
 
   def self.set_player_sign
@@ -36,10 +37,17 @@ class TicTacToe
     end
   end
 
-  def self.board_status; end
-  def self.display_board; end
+  def self.board
+    @board = [%w[_ _ _], %w[_ _ _], %w[_ _ _]]
+  end
+
+  def self.display_board
+    @board.each { |row| puts " #{row[0]} | #{row[1]} | #{row[2]}" }
+  end
+
   def self.make_turn; end
   def self.check_win_condition; end
+  # theodinproject => nested collection => iteration over a nested array
   def self.check_row; end
   def self.check_column; end
   def self.check_diagonal; end
@@ -52,6 +60,7 @@ class TicTacToe
     puts @player2_sign
     puts @computer_mode
     puts @computer_sign
+    display_board
   end
 end
 
