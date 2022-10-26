b2f3d09b0efe13d91322db36f37a6f5bff0e069a
dpanhey <d.panhey@gmx.de>
2022-10-26 12:06:51 +0200

    initial commit, set up core methods

---
+++ fullstack_ruby/01_ruby_programming/ruby-projects/tictactoe.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+# Top level Documentation for class TicTacToe
+class TicTacToe
+  def self.start_game; end
+  def self.set_players; end
+  def self.display_board; end
+  def self.make_turn; end
+  def self.check_win_condition; end
+  def self.end_game; end
+end
