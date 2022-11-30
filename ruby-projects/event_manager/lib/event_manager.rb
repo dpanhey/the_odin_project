001748e6cabd9d12b1772d85ba8c2279c29ca697
dpanhey <d.panhey@gmx.de>
2022-11-30 15:52:44 +0100

    added form letter file

--- ruby-projects/event_manager/lib/event_manager.rb
+++ ruby-projects/event_manager/lib/event_manager.rb
@@ -1,3 +1,37 @@
 # frozen_string_literal: true
 
+require 'csv'
+require 'google/apis/civicinfo_v2'
+
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5, '0').slice(0..4)
+end
+
+def legislator_by_zipcode(zipcode)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+
+  civic_info.representative_info_by_address(
+    address: zipcode,
+    levels: 'country',
+    roles: %w[legislatorUpperBody legislatorLowerBody]
+  ).officials.map(&:name).join(', ')
+rescue StandardError
+  'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+end
+
 puts 'Event Manager Initialized!'
+
+contents = CSV.open(
+  'event_attendees.csv',
+  headers: true,
+  header_converters: :symbol
+)
+
+contents.each do |row|
+  name = row[:first_name]
+  zipcode = clean_zipcode(row[:zipcode])
+  legislators = legislator_by_zipcode(zipcode)
+
+  puts "#{name} #{zipcode} #{legislators}"
+end
