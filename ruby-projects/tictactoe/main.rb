70dd4af86af6fbb3039d96d524f2a97be24976b1
dpanhey <d.panhey@gmx.de>
2022-12-03 15:58:12 +0100

    added TicTacToe class with intro method to begin the game

--- ruby-projects/tictactoe/main.rb
+++ ruby-projects/tictactoe/main.rb
@@ -2,3 +2,20 @@
 
 require 'require_all'
 require_all 'lib'
+
+# The main class for bundling all classes and modules and starting the game
+class TicTacToe
+  extend DisplayText
+  @text_output = { start_message: [
+    'Welcome to TicaTacToe!',
+    'To start, please type in your names one after another!'
+  ] }
+
+  def self.intro
+    display_multiline_string(@text_output[:start_message])
+    @players = Player.new
+    display_string("Alright, lets begin with #{@players.players[0][:name]}!")
+  end
+end
+
+TicTacToe.intro
