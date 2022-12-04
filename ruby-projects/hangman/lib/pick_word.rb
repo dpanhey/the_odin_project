00a3958f6b7b4cdbf01ded126635933bab685d81
dpanhey <d.panhey@gmx.de>
2022-12-04 18:46:03 +0100

    added method for saving all strings with 5 to 12 characters from file to array

--- ruby-projects/hangman/lib/pick_word.rb
+++ ruby-projects/hangman/lib/pick_word.rb
@@ -3,4 +3,16 @@
 # The module for choosing a random Word from a text-file
 module PickWord
   def initialize; end
+
+  def self.load_file
+    @content = []
+    @file = File.open('../google-10000-english-no-swears.txt', 'r')
+    until @file.eof?
+      @line = @file.readline.chomp
+      @content << @line if @line.length.between?(5, 12)
+    end
+    p @content
+  end
 end
+
+PickWord.load_file
