ec74448afb7eaa2a64b1a0633a020846ae4434fe
dpanhey <d.panhey@gmx.de>
2022-12-05 14:58:28 +0100

    added method to initialize variables to Game class

--- ruby-projects/tictactoe/lib/game.rb
+++ ruby-projects/tictactoe/lib/game.rb
@@ -3,10 +3,22 @@
 # The class for playing the game
 class Game
   include DisplayText
+  include TurnLogic
+
+  def initialize
+    intro
+    initialize_variables
+  end
 
   def intro
     intro_message
     @players = Player.new
     start_message
+  end
+
+  def initialize_variables
+    @board = Boardlogic.new
+    @turn = 1
+    @current_player_id = 0
   end
 end
