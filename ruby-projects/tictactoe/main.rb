e9e2d42064b4947dd85f1c88bd29880ecd77e3ee
dpanhey <d.panhey@gmx.de>
2022-12-05 14:08:51 +0100

    redefined class with instance methods

--- ruby-projects/tictactoe/main.rb
+++ ruby-projects/tictactoe/main.rb
@@ -5,17 +5,21 @@ require_all 'lib'
 
 # The main class for bundling all classes and modules and starting the game
 class TicTacToe
-  extend DisplayText
-  @text_output = { start_message: [
-    'Welcome to TicaTacToe!',
-    'To start, please type in your names one after another!'
-  ] }
+  include DisplayText
 
-  def self.intro
+  def initialize
+    @text_output =
+      { start_message: [
+        'Welcome to TicaTacToe!',
+        'To start, please type in your names one after another!'
+      ] }
+  end
+
+  def intro
     display_multiline_string(@text_output[:start_message])
     @players = Player.new
     display_string("Alright, lets begin with #{@players.players[0][:name]}!")
   end
 end
 
-TicTacToe.intro
+TicTacToe.new.intro
