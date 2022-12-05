f553a19ae1248a02c43a6176a6e5b9f874999961
dpanhey <d.panhey@gmx.de>
2022-12-05 14:57:43 +0100

    added player logic to TurnLogic

--- ruby-projects/tictactoe/lib/turn_logic.rb
+++ ruby-projects/tictactoe/lib/turn_logic.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
 
 # The module for the whole turn logic of TicTacToe
-module TurnLogic; end
+module TurnLogic
+  def other_player_id
+    1 - @current_player_id
+  end
+
+  def switch_player!
+    @current_player_id = other_player_id
+  end
+
+  def current_player
+    @player[current_player_id]
+  end
+end
