5d9948f56b73c1a2a42b718ebda67dd4624deefd
dpanhey <d.panhey@gmx.de>
2022-12-06 23:19:48 +0100

    added input method and the validation for that input

--- ruby-projects/hangman/lib/game_logic.rb
+++ ruby-projects/hangman/lib/game_logic.rb
@@ -9,12 +9,29 @@ class GameLogic
   include PickWord
   # the player is allowed 7 mistakes to make
   def initialize
-    initialize_words
+    initialize_variables
+    input
   end
 
-  def initialize_words
+  def initialize_variables
     @guessing_word = [] << choose_random_word('google-10000-english-no-swears.txt')
     @displaying_word = Array.new(1) { '_' * @guessing_word[0].length }
     @displaying_word[0].gsub!(/(.{1})(?=.)/, '\1 \2')
+    @guessed_characters = []
+  end
+
+  def input
+    display_string('Please type in one alphabetical character that you didnt choose yet!')
+    @input = gets.chomp.downcase
+    validate_input
+  end
+
+  def validate_input
+    @input = gets.chomp.downcase until
+      @input.length == 1 &&
+      @input.match?(/[[:alpha:]]/) &&
+      !@displaying_word.include?(@input)
+    @guessed_characters << @input
+    @guessed_characters[0].gsub!(/(.{1})(?=.)/, '\1 \2')
   end
 end
