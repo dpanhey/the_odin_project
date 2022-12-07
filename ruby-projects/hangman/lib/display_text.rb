db91189f7daf7649c8d8b6f71dd02a84296921a7
dpanhey <d.panhey@gmx.de>
2022-12-07 14:53:02 +0100

    added method to display guessed characters

--- ruby-projects/hangman/lib/display_text.rb
+++ ruby-projects/hangman/lib/display_text.rb
@@ -5,7 +5,7 @@ module DisplayText
   def display_word_sleep(word)
     word.each_char do |char|
       print char
-      sleep 0.05
+      sleep 0.025
       $stdout.flush
     end
   end
@@ -13,6 +13,7 @@ module DisplayText
   def display_string(string)
     display_word_sleep(string)
     print "\n"
+    sleep 0.3
   end
 
   def display_multiline_string(array)
@@ -25,15 +26,20 @@ module DisplayText
 
   def display_turn_text
     display_mistake_counter
+    display_guessed_characters
     display_displaying_word
   end
 
   def display_mistake_counter
-    puts "Mistakes: #{@game_content[:guesses][:mistakes_count]} of 7!"
+    display_string("Mistakes: #{@game_content[:guesses][:mistakes_count]} of 7!")
   end
 
   def display_displaying_word
-    puts "Your guesses word so far: #{@game_content[:display_displaying_word][0]}"
+    display_string("Your guessed word so far: #{@game_content[:displaying_word][0]}")
+  end
+
+  def display_guessed_characters
+    display_string("Your guessed characters so far: #{@game_content[:guessed_characters]}")
   end
 
   def intro_message
@@ -43,13 +49,13 @@ module DisplayText
   end
 
   def winning_message
-    display_multiline_string(["Yeah, #{@game_content[:guessing_word][0]} is the right answer!",
+    display_multiline_string(["Yes, '#{@game_content[:guessing_word][0]}' is the right answer!",
                               "You won after #{@game_content[:guesses][:guesses_count]} guesses and "\
                               "#{@game_content[:guesses][:mistakes_count]} mistakes!"])
   end
 
   def loosing_message
-    display_multiline_string(["Oh no, that was your last guess for the word: #{@game_content[:guessing_word][0]}!",
+    display_multiline_string(["Oh no, that was your last guess for the word: '#{@game_content[:guessing_word][0]}'!",
                               "You loose after #{@game_content[:guesses][:guesses_count]} guesses and "\
                               "#{@game_content[:guesses][:mistakes_count]} mistakes!"])
   end
