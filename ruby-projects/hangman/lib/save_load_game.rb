a72321962ae97e42e96f9f37bf3a3c6e3bfa6915
dpanhey <d.panhey@gmx.de>
2022-12-12 17:54:25 +0100

    created methods to save a game with json to a textfile, load it from a given filename and ask for this methods

--- ruby-projects/hangman/lib/save_load_game.rb
+++ ruby-projects/hangman/lib/save_load_game.rb
@@ -4,9 +4,32 @@ require 'json'
 
 # the module for the saving and loading methods for hangman
 module SaveLoadGame
-  def ask_for_load; end
+  def ask_for_load
+    puts "If you want to load a savegame, type 'load'."
+    load_file(ask_for_filename) if gets.chomp == 'load'
+  end
 
-  def ask_for_save; end
+  def ask_for_filename
+    puts 'Type in the name of your file.'
+    gets.chomp
+  end
+
+  def load_file(filename)
+    File.exist?(filename) ? from_json(File.read(filename)) : 'No such File detected!'
+  end
+
+  def ask_for_save
+    puts "If you want to save this game, type 'save'."
+    save_file if gets.chomp == 'save'
+  end
+
+  def save_file
+    Dir.mkdir('../savegames') unless Dir.exist?('../savegames')
+
+    savegame_name = "savegame_#{@game_content[guessing_word[0]]}.txt"
+    File.open("../#{savegame_name}", 'w') { |file| file.puts to_json }
+    puts "File #{savegame_name} saved sucessfull! You may close this game."
+  end
 
   def to_json(*_args)
     content = Hash.new { |hash, key| hash[key] = hash.dup.clear }
@@ -21,8 +44,4 @@ module SaveLoadGame
       instance_variable_set variable, value
     end
   end
-
-  def save_file; end
-
-  def load_file(); end
 end
